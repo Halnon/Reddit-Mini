179563a5f64c1e8f067a3fe476bb2e4a88c6563e
Adam Halnon <halnon_01@hotmail.com>
2024-07-30 17:00:40 +0100

    add initial jsx to render h1 element

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+//navigation bar with title of page at the top
+
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <header className="">
+          <h1>React Mini</h1>
+        </header>
+    )
+};
