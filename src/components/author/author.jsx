b987a31b8ef6176084d7e57455db3c39cb61dfc1
Adam Halnon <halnon_01@hotmail.com>
2024-08-01 17:18:45 +0100

    create and export author component

---
+++ src/components/author/author.jsx
@@ -0,0 +1,11 @@
+//this will display the name of the original poster
+
+import React from "react";
+
+export default function Author() {
+  return (
+      <div>
+        <p>Posted by <a href="#">Adam</a></p>
+      </div>
+  )
+};
