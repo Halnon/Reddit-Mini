56dcbe32678515576f45338a6dadee13396318f1
Adam Halnon <halnon_01@hotmail.com>
2024-08-01 17:34:52 +0100

    create and export time component

---
+++ src/components/time/time.jsx
@@ -0,0 +1,11 @@
+//this will display how long ago the content was posted
+
+import React from "react";
+
+export default function Time() {
+  return (
+      <div>
+        <p>10 hours ago</p>
+      </div>
+  )
+};
