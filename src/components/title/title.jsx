ee3ed6500804bfefeb7dac494bd62c22b41c375c
Adam Halnon <halnon_01@hotmail.com>
2024-08-01 16:20:35 +0100

    create and export title comp

---
+++ src/components/title/title.jsx
@@ -0,0 +1,11 @@
+//fist component to be rendered in maintile. this will display the title of the reddit content
+
+import React from "react";
+
+export default function Title() {
+  return (
+      <div className="title--container">
+        <h2>Main title of content</h2>
+      </div>
+  )
+};
