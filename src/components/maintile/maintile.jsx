5c36928583e7078cc9c1aa55f441436ca4ea117c
Adam Halnon <halnon_01@hotmail.com>
2024-08-01 15:37:29 +0100

    create and export function Maintile

---
+++ src/components/maintile/maintile.jsx
@@ -0,0 +1,12 @@
+//all other components will be rendered within this, and in turn, this will be rendered in app.js
+//multiple tiles will be rendered on page that displays various content from Reddit JSON API
+
+import React from "react";
+
+export default function Maintile() {
+  return (
+      <div className="maintile--container">
+        <h2>hello</h2>
+      </div>
+  )
+};
