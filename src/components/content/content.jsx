b34f30cc313f7f3dd8581d7f06a55f98686c7313
Adam Halnon <halnon_01@hotmail.com>
2024-08-01 16:29:05 +0100

    create and export content component

---
+++ src/components/content/content.jsx
@@ -0,0 +1,11 @@
+//this will display the content and will be the second component to be rendered in maintile
+
+import React from "react";
+
+export default function Content() {
+  return (
+      <div className="content">
+        <img src="#"/>
+      </div>
+  )
+};
