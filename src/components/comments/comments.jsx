5091f6b4689b1a242c790ce5c4675303967ec79c
Adam Halnon <halnon_01@hotmail.com>
2024-08-01 17:39:42 +0100

    create and export comments component

---
+++ src/components/comments/comments.jsx
@@ -0,0 +1,11 @@
+//this will display the amount of comments on a post
+
+import React from "react";
+
+export default function Comments() {
+  return (
+      <div>
+        <p>100</p>
+      </div>
+  )
+};
