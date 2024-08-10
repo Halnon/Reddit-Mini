c7163fcc9ca28e1359e428aebff43b378db2e4b4
Adam Halnon <halnon_01@hotmail.com>
2024-08-10 18:49:25 +0100

    import data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Navbar from './components/navbar/navbar';
 import Maintile from './components/maintile/maintile';
 import './App.css';
+import data from './data.js';
 
 function App() {
 
