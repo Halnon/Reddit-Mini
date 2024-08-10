91223811ae120b2c354b763ff914b0fc0f949473
Adam Halnon <halnon_01@hotmail.com>
2024-08-10 18:48:27 +0100

    add sample data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,35 @@
 //sample test data to for components
 
-export default [];
+export default [
+  {
+    id: 1,
+    title: 'Adam & Tiz',
+    contentImg: 'AdTiz.jpg',
+    votes: '1000',
+    author: 'Tiz',
+    time: '7 hous ago',
+    comments: "37"
+  },
+  {
+    id: 2,
+    title: '',
+    contentImg: '',
+    votes: '',
+    author: '',
+    time: '',
+    comments: ""
+  },
+  {
+    id: 3,
+    title: '',
+    contentImg: '',
+    votes: '',
+    author: '',
+    time: '',
+    comments: ""
+  },
+
+];
 
 /*
 template object
