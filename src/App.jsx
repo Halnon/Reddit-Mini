7bec258bd2f66922d056132585e9322fa543bde8
Adam Halnon <halnon_01@hotmail.com>
2024-08-10 19:04:37 +0100

    render data using map method and pass on props to components

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import './App.css';
 import data from './data.js';
 
 function App() {
+  const MaintileMap = data.map((data) => {
+    return <Maintile
+              key={data.id}
+              title={data.title}
+              contentImg={data.contentImg}
+              votes={data.votes}
+              author={data.author}
+              time={data.time}
+              comments={data.comments}
+            />
+  })
 
   return (
     <div>
       <Navbar />
-      <Maintile 
-        key={data.id}
-        title={data.title}
-        contentImg={data.contentImg}
-        votes={data.votes}
-        author={data.author}
-        time={data.time}
-        comments={data.comments}
-      />
+      {MaintileMap}
     </div>
   )
 }
