a9b1a2251406ad4429a9d4df404a24e4844893c3
Adam Halnon <halnon_01@hotmail.com>
2024-08-10 19:43:28 +0100

    receive props

--- src/components/votes/votes.jsx
+++ src/components/votes/votes.jsx
@@ -5,11 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
-export default function Votes() {
+export default function Votes(props) {
   return (
       <div>
         <button><FontAwesomeIcon icon={faArrowUp} /></button>
-        <p>35.5k</p>
+        <p>{props.votes}</p>
         <button><FontAwesomeIcon icon={faArrowDown} /></button>
       </div>
   )
