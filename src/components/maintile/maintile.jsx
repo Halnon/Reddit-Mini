9f34992e37590157b13158429874c27e1c0a14d0
Adam Halnon <halnon_01@hotmail.com>
2024-08-10 18:31:13 +0100

    import and render votes comp

--- src/components/maintile/maintile.jsx
+++ src/components/maintile/maintile.jsx
@@ -7,10 +7,14 @@ import Content from "../content/content";
 import Author from "../author/author";
 import Time from "../time/time";
 import Comments from "../comments/comments";
+import Votes from "../votes/votes";
 
 export default function Maintile() {
   return (
       <div className="maintile--container">
+        <div>
+          <Votes />
+        </div>
         <div className="maintile--container--2">
           <Title />
           <Content />
